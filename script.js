e34e2b054fd0c9a976f06b16400d90406f54e8f7
whenyouword <e11iottaj1165@gmail.com>
2025-05-07 19:45:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scroll for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
